e055f8208b8f935fa43634b543d9c083f9bece36
Ben Williams and Jason Ainsworth <ben@>
2017-05-23 10:52:53 -0700

    add working weather and fully updated project

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,14 +1,13 @@
-var apiKey = "e6fe13f85771d8fd9bf0ec55a79d0d67";
+var Weather = require('./../js/weather.js').weatherModule;
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-
-
-    });
+    currentWeatherObject.getWeather(city, displayHumidity);
   });
-});
+ });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -62,17 +62,16 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = "e6fe13f85771d8fd9bf0ec55a79d0d67";
+var Weather = require('./../js/weather.js').weatherModule;
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response.main.humidity);
-      // $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-
-
-    });
+    currentWeatherObject.getWeather(city, displayHumidity);
   });
-});
+ });
